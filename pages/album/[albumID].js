27474cb5e12f6c57f2096dd68f7ef89c8f281827
mordisco <119074601+mordisco@users.noreply.github.com>
2022-11-25 19:59:43 +0100

    Add files via upload

---
+++ pages/album/[albumID].js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { albums } from 'data'
+import { getImagesFromFolder } from '@/lib/cloudinary/directory.js'
+import Layout from '@/app/Layout'
+import Modal from '@/app/Modal'
+import BigPhoto from '@/ui/BigPhoto'
+import Albums from '@/ui/Albums'
+import Grid from '@/ui/Grid'
+import AlbumTitle from '@/ui/AlbumTitle'
+
+// getStaticPaths
+export async function getStaticPaths() {
+  const paths = albums.map((album) => ({
+    params: { albumID: album.folder },
+  }))
+
+  return { paths, fallback: false }
+}
+
+// getStaticProps
+export async function getStaticProps({ params }) {
+  const { albumID } = params
+  const album = albums.find((album) => album.folder === albumID)
+  const images = await getImagesFromFolder(albumID)
+  return { props: { albumID, album, images } }
+}
+
+export default function Album({ albumID, album, images }) {
+  const [showModal, setModal] = useState()
+  const [modalImage, setModalImage] = useState()
+
+  if (!images) return null
+
+  function openModal(photo) {
+    setModalImage(photo)
+    setModal(true)
+  }
+
+  function closeModal() {
+    setModal(false)
+    setModalImage(null)
+  }
+
+  return (
+    <Layout>
+      <Modal visible={showModal} close={() => closeModal()}>
+        <BigPhoto photo={modalImage} />
+      </Modal>
+
+      <Albums className='mb-8' albums={albums} activeID={albumID} />
+
+      <AlbumTitle album={album} />
+      <Grid items={images} openModal={openModal} />
+    </Layout>
+  )
+}
