76ca56e7b53ff943fce23be16d7fc4bc27511715
Manu Mordisco <manu@mordisco.club>
2022-11-26 03:14:57 +0100

    Add xs screen breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ['./pages/**/*.js', './components/**/*.js'],
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
       spacing: {
         'header-sm': '60px',
