7400147e1164573428039f8fdd039225a953dad4
Manu Morante <manu@estadologico.com>
2025-01-12 23:49:35 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <div className="Header pt-10 pb-8 flex justify-center items-center">
+    <div className="Header py-10 flex justify-center items-center">
       <Link href="/">
         <Logo className="w-36 h-auto" />
       </Link>
